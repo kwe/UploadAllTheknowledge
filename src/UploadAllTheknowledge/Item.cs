91ac027454d41ef09d10edab5b939eee05a0eeee
kwe <kwevans@gmail.com>
2016-07-04 15:28:58 +0100

    Add a simgle item

---
+++ src/UploadAllTheknowledge/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace UploadAllTheknowledge
+{
+    public class Item
+    {
+        public String Id { get; set; }
+        public String Title { get; set; }
+        public String Summary { get; set; }
+    }
+}
--- src/UploadAllTheknowledge/Program.cs
+++ src/UploadAllTheknowledge/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Nest;
 
 namespace UploadAllTheknowledge
 {
@@ -9,6 +7,28 @@ namespace UploadAllTheknowledge
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Hello kwe");
+
+            var local = new Uri("http://docker:9200");
+            var setting = new ConnectionSettings(local).DefaultIndex("default-index");
+            var client = new ElasticClient(setting);
+
+            var res = client.ClusterHealth();
+
+            Console.WriteLine(res.Status);
+
+            var item = new Item
+            {
+                Id = "1",
+                Title = "Sample Document",
+                Summary = "A lovely test document to add to the Elasticsearch collection"
+            };
+
+            var index = client.Index(item);
+
+
+            // http://docker:9200/default-index/_search to view what's happened.
+
         }
     }
 }
